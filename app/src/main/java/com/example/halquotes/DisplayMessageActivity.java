fdc904cf9b7a9382490ea81931c2344bb6a8e05a
ihopethisnamefi <jonthibeault@gmail.com>
2017-02-09 20:24:01 -0600

    Pictures + Quote Bubble
    Added random hal pictures and a quote bubble

--- app/src/main/java/com/example/halquotes/DisplayMessageActivity.java
+++ app/src/main/java/com/example/halquotes/DisplayMessageActivity.java
@@ -4,16 +4,25 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Button;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -35,26 +44,46 @@ public class DisplayMessageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* create a full screen window */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_display_message);
 
+        /* adapt the image to the size of the display */
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.hal_01),size.x,size.y,true);
+
+        /* fill the background ImageView with the resized image */
+        ImageView iv_background = (ImageView) findViewById(R.id.iv_background);
+        iv_background.setImageBitmap(bmp);
+
+        /* define quotes array and buttons */
         quotes = getResources().getStringArray(R.array.quotes_array);
         restart = (Button)findViewById(R.id.RestartButton);
         clear = (Button)findViewById(R.id.ClearButton);
 
+        /* begin sound resource */
         mp = MediaPlayer.create(context, R.raw.fart01);
 
+        /* message and textview initialization and attributes */
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         textView = new TextView(this);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.BLACK);
+        textView.setBackgroundColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER_VERTICAL);
 
+        /* create random for quote array */
         Random rand = new Random();
         int num = rand.nextInt(quotes.length);
         String quote = quotes[num];
         textView.setText(quote);
         quotes = ArrayUtils.remove(quotes, num);
 
+        /* insert first quote */
         textView.setText(quote);
 
         //quotes=getResources().getStringArray(R.array.quotes_array);
@@ -77,10 +106,27 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         if (quotes.length > 0) {
 
+            Display display = getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+
+
+            int[] halpics = {R.drawable.hal_01, R.drawable.hal_02, R.drawable.hal_03, R.drawable.hal_04, R.drawable.hal_05};
+            Random randpics = new Random();
+            int numpics = randpics.nextInt(halpics.length);
+            int halpic = halpics[numpics];
+            Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),halpic),size.x,size.y,true);
+
+            /* fill the background ImageView with the resized image */
+            ImageView iv_background = (ImageView) findViewById(R.id.iv_background);
+            iv_background.setImageBitmap(bmp);
+
+            /* Grab new random quote from the array list */
             Random rand = new Random();
             int num = rand.nextInt(quotes.length);
             String quote = quotes[num];
             textView.setText(quote);
+            /*remove quotes from the Array so it's only used once */
             quotes = ArrayUtils.remove(quotes, num);
 
         } else {
